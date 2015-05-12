5b43a1032ec1a5076dc887a56682b2c9aad2802c
Eric Andrade <eric.oliveira.andrade@gmail.com>
2015-05-12 18:25:24 -0300

    Adicionando campos para tratamento de entrada de datas, arquivos, email, telefone, cor, range

--- hello.php
+++ hello.php
@@ -1,5 +1,12 @@
-<?php 
-
-print_r($_POST);
-
+<!DOCTYPE html>
+<html>
+<head><meta charset="utf-8"><meta name="viewport" content="width=device-width">
+<body>
+<?php
+    echo "<pre>";
+    print_r($_POST);
+    echo "</pre>";
 ?>
+</body>
+</html>
+
